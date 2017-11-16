5ad72b4046bcbdb75eb5339da9b1a57b3bf7032b
Oleh Dubno <olehdubno@gmail.com>
2017-11-15 19:39:18 -0500

    add a unit test

---
+++ test_naive_bayes.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import unittest
+from naive_bayes import GaussNB
+
+
+class GaussNBTest(TestCase):
+
+    def setUp(self):
+        self.nb = GaussNB()
+
+    def test_split_data(self):
+        dataset = [[1], [2], [3], [4], [5]]
+        splitRatio = 0.70
+        train, test = self.nb.split_data(dataset, splitRatio)
+        self.assertEqual(3, len(train))
+        self.assertEqual(2, len(test))
+
+if __name__ == '__main__':
+    unittest.main()
